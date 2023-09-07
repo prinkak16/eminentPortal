d8429e4d007955f9a16cb61c494ba0080657dd70
leo <leo@leos-MacBook-Pro.local>
2023-09-07 10:33:34 +0530

    remove the routes sso and locatable

--- config/routes.rb
+++ config/routes.rb
@@ -3,9 +3,6 @@ require 'sidekiq/cron/web'
 Rails.application.routes.draw do
   root "home#index"
   # Define your application modules per the DSL in https://guides.rubyonrails.org/routing.html
-
-  mount SsoClient::Engine, at: '/sso_client', as: 'sso_client'
-  mount Saral::Locatable::Engine => "/saral-locatable", :as => 'saral_locatable'
   mount Sidekiq::Web => "/sidekiq"
 
   #noinspection RailsParamDefResolve
