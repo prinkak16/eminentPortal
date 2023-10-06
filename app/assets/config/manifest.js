9afc3dd1c35086ce9438438574fe72400a582460
leo <leo@leos-MacBook-Pro.local>
2023-10-06 13:58:55 +0530

    changes for the remove dependency

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -1,8 +1,5 @@
 //= link_tree ../images
 //= link_tree ../builds
-//= link saral/locatable/application.css
-//= link sso_client/application.css
-//= link sso_client/application.js
 //= link application.css
 
 
