386254417d80b0425255662b6d4d57fe39a7c7b5
leo <leo@leos-MacBook-Pro.local>
2023-10-11 13:00:47 +0530

    changes for the metadata helper

---
+++ app/helpers/metadata_helper.rb
@@ -0,0 +1,172 @@
+module MetadataHelper
+  include ApplicationHelper
+
+  def get_entry_type_filter
+    result = {
+      'type': 'array',
+      'values': []
+    }
+
+    fetch_user_assigned_country_states.each do |country_state|
+      result[:values] << {
+        'value': country_state['id'],
+        'display_name': country_state['name']
+      }
+    end
+    result
+  end
+
+  def get_channel_filter
+    {
+      'type': 'array',
+      'values': [
+        {
+          'value': 'Office',
+          'display_name': 'Office'
+        },
+        {
+          'value': 'Link',
+          'display_name': 'Link'
+        }
+      ]
+    }
+  end
+
+  def get_aasm_state_filter
+    {
+      'type': 'array',
+      'values': [
+        {
+          'value': 'pending',
+          'display_name': 'Pending'
+        },
+        {
+          'value': 'otp_verified',
+          'display_name': 'OTP Verified'
+        },
+        {
+          'value': 'incomplete',
+          'display_name': 'Incomplete'
+        },
+        {
+          'value': 'submitted',
+          'display_name': 'Submitted'
+        },
+        {
+          'value': 'rejected',
+          'display_name': 'Re-edit'
+        },
+        {
+          'value': 'approved',
+          'display_name': 'Freeze'
+        }
+      ]
+    }
+  end
+
+  def get_qualification_filter
+    result = {
+      'type': 'array',
+      'values': []
+    }
+    educations = PersonEducation.order(:order).select(:id, :name)
+    educations.each do |education|
+      result[:values] << {
+        'value': education.name,
+        'display_name': education.name
+      }
+    end
+    result
+  end
+
+  def get_gender_filter
+    result = {
+      'type': 'array',
+      'values': [
+        {
+          'value': 'Male',
+          'display_name': 'Male'
+        },
+        {
+          'value': 'Female',
+          'display_name': 'Female'
+        },
+        {
+          'value': 'Others',
+          'display_name': 'Others'
+        }
+      ]
+    }
+  end
+
+  def get_profession_filter
+    result = {
+      'type': 'array',
+      'values': []
+    }
+    professions = PersonProfession.select(:id, :name)
+    professions.each do |profession|
+      result[:values] << {
+        'value': profession.name,
+        'display_name': profession.name
+      }
+    end
+    result
+  end
+
+  def get_category_filter
+    result = {
+      'type': 'array',
+      'values': []
+    }
+    condition = "CASE person_categories.name WHEN 'GEN' THEN '1' WHEN 'OBC' THEN '2' WHEN 'SC' THEN '3' WHEN 'ST' THEN '4' WHEN 'Minority' THEN '5' else 100 END"
+    categories = PersonCategory.all.order(Arel.sql(condition)).select(:id, :name)
+    categories.each do |category|
+      result[:values] << {
+        'value': category.name,
+        'display_name': category.name
+      }
+    end
+    result
+  end
+
+  def get_age_group_filter
+    result = {
+      'type': 'array',
+      'values': [
+        {
+          'value': 'age_18_to_25',
+          'display_name': '18-25'
+        },
+        {
+          'value': 'age_25_to_30',
+          'display_name': '25-30'
+        },
+        {
+          'value': 'age_30_to_35',
+          'display_name': '30-35'
+        },
+        {
+          'value': 'age_35_to_40',
+          'display_name': '35-40'
+        },
+        {
+          'value': 'age_40_to_45',
+          'display_name': '40-45'
+        },
+        {
+          'value': 'age_45_to_50',
+          'display_name': '45-50'
+        },
+        {
+          'value': 'age_50_to_65',
+          'display_name': '50-65'
+        },
+        {
+          'value': 'age_greater_65',
+          'display_name': '65+'
+        }
+      ]
+    }
+  end
+end
