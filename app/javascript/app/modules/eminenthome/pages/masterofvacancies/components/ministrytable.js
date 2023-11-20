29d4a24a47ebd5e45652709a9838aec9c7697ded
priyanka <priyanka.kamboj@jarvis.consulting>
2023-11-20 15:38:37 +0530

    ministry table api integration done

--- app/javascript/app/modules/eminenthome/pages/masterofvacancies/components/ministrytable.js
+++ app/javascript/app/modules/eminenthome/pages/masterofvacancies/components/ministrytable.js
@@ -1,11 +1,5 @@
 import * as React from 'react';
-import Table from '@mui/material/Table';
-import TableBody from '@mui/material/TableBody';
-import TableCell from '@mui/material/TableCell';
-import TableContainer from '@mui/material/TableContainer';
-import TableHead from '@mui/material/TableHead';
-import TableRow from '@mui/material/TableRow';
-import Paper from '@mui/material/Paper';
+import {Table, TableBody,TableCell, TableContainer,TableHead,  TableRow,Paper, Backdrop, CircularProgress } from '@mui/material'
 import {useEffect, useState} from "react";
 import ReactPaginate from "react-paginate";
 import {getMinistryWiseData} from "../../../../../api/eminentapis/endpoints";
@@ -16,7 +10,6 @@ const  MinistryTable = ({onSwitchTab}) => {
     const [ministryTableData, setMinistryTableData] = useState([]);
     const [isFetching, setIsFetching] = useState(false);
     const [error, setError] = useState(null);
-
     const prepareToGetDisplayData = () => {
         // let pageString = '';
         // let offset = currentPage * limit;
@@ -35,7 +28,6 @@ const  MinistryTable = ({onSwitchTab}) => {
             .then(response => {
             setIsFetching(false);
             setMinistryTableData(response.data.data.value);
-
         }).catch(error => {
             setIsFetching(false);
             setError(error);
@@ -46,7 +38,15 @@ const  MinistryTable = ({onSwitchTab}) => {
 
     return (
         <>
-            {/*{isFetching && <h1>Data is fetching</h1>}*/}
+
+                <Backdrop
+                    sx={{ color: '#fff', zIndex: (theme) => theme.zIndex.drawer + 1 }}
+                    open={isFetching}
+                >
+                    <CircularProgress color="inherit" />
+                </Backdrop>
+
+
         <TableContainer component={Paper} className="psutable">
             <Table sx={{ minWidth: 650 }} aria-label="simple table">
                 <TableHead>
