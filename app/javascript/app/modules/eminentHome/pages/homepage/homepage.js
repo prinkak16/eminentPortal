fb9f1ff34534b51e37ef97975f536aedf9369856
Vijay Gurjar <vijay.kumar@jarvis.consulting>
2023-12-05 14:48:26 +0530

    'open list functionality works fine'

--- app/javascript/app/modules/eminentHome/pages/homepage/homepage.js
+++ app/javascript/app/modules/eminentHome/pages/homepage/homepage.js
@@ -125,7 +125,7 @@ export default function PersistentDrawerLeft() {
     useEffect(() => {
         userPermissions().then(
             (res) => {
-              console.log(res.data)
+
             }
         )
     },[])
