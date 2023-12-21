0e58805eadb8170e4197656faa7a9c0146b74a95
priyanka <priyanka.kamboj@jarvis.consulting>
2023-12-21 12:27:33 +0530

    profile page api integration done

--- app/javascript/app/modules/eminentHome/pages/profile/profilePage.jsx
+++ app/javascript/app/modules/eminentHome/pages/profile/profilePage.jsx
@@ -1,6 +1,8 @@
 import * as React from 'react';
 import { styled } from '@mui/material/styles';
 import {Box, Typography, Paper, Grid} from '@mui/material';
+import {getProfileData} from "../../../../api/eminentapis/endpoints";
+import {useEffect, useState} from "react";
 
 
 const profileStyle = {
@@ -14,36 +16,70 @@ const profileStyle = {
         span:{
             fontSize:'1rem',
         }
+    },
+    permission:{
+        span:{
+            marginRight:'9px'
+        }
     }
 }
 
 const Profile = () => {
+    const [profileDetail, setProfileDetail] = useState(null)
+    const [permissions, setPermissions] = useState([]);
+    const [allottedStates, setAllottedStates] = useState([]);
+    const [ministries, setMinistries] = useState([]);
+    const profileData = () => {
+        getProfileData().then((response) => {
+            setProfileDetail(response.data.data)
+            setPermissions(response.data.data.permissions)
+            setAllottedStates(response.data.data.allotted_states)
+            setMinistries(response.data.data)
+        })
+    }
+    useEffect(() => {
+        profileData()
+    }, []);
+    console.log('permissions', permissions)
     return (
         <Box sx={profileStyle}>
             <Grid container spacing={2} className="justify-content-center">
-                <Grid item xs={12} md={7}>
+                {profileDetail && (
+                    <Grid item xs={12} md={7}>
                         <Grid sx={profileStyle.card}>
-                            <Typography variant="p"><b>Name:</b></Typography>
+                            <Typography variant="p"><b>Name:</b> {profileDetail.name}</Typography>
                         </Grid>
                         <Grid sx={profileStyle.card}>
-                            <Typography variant="p"><b>Email:</b></Typography>
+                            <Typography variant="p"><b>Email:</b> {profileDetail.email}</Typography>
                         </Grid>
                         <Grid sx={profileStyle.card}>
-                            <Typography variant="p"><b>Phone:</b></Typography>
+                            <Typography variant="p"><b>Phone:</b> {profileDetail.phone}</Typography>
                         </Grid>
                         <Grid sx={profileStyle.card}>
-                            <Typography variant="p"><b>Role:</b></Typography>
+                            <Typography variant="p"><b>Role:</b> {profileDetail.role}</Typography>
                         </Grid>
                         <Grid sx={profileStyle.card}>
-                            <Typography variant="p"><b>Permissions:</b></Typography>
+                            <Typography variant="p" sx={profileStyle.permission}><b>Permissions: </b>
+                                {permissions.map(permission => (
+                                   <span>{permission.action} - {permission.permission_name},</span>
+                                ))}
+                            </Typography>
                         </Grid>
                         <Grid sx={profileStyle.card}>
-                            <Typography variant="p"><b>Allotted States:</b></Typography>
+                            <Typography variant="p"><b>Allotted States:</b>
+                                {allottedStates.map(states => (
+                                    <span> {states.name},</span>
+                                ))}
+                            </Typography>
                         </Grid>
                         <Grid sx={profileStyle.card}>
                             <Typography variant="p"><b>Allotted Ministries:</b></Typography>
-                        </Grid> 
-                </Grid>
+                        </Grid>
+                    </Grid>
+                )}
+
+
+
             </Grid>
         </Box>
     );
