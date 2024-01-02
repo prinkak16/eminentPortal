21c75bce278f546a4512f306b2bf1553055e2358
Chitranshoo Prakash <chitranshoo.prakash@jarvis.consulting>
2024-01-02 14:58:45 +0530

    commit

--- app/javascript/app/modules/eminentHome/shared/tabs/tabs.jsx
+++ app/javascript/app/modules/eminentHome/shared/tabs/tabs.jsx
@@ -34,7 +34,6 @@ import FileStatus from "../../pages/fileStatus/fileStatus";
 import Tabs from "@mui/material/Tabs";
 import {checkPermission, isValuePresent, convertToCamelCase} from "../../../utils";
 import AllotmentContext from "../../pages/allotment/context/allotmentContext";
-import {getUserPermissions} from "../../../../api/stepperApiEndpoints/stepperapiendpoints";
 
 const VisuallyHiddenInput = styled("input")({
   clip: "rect(0 0 0 0)",
@@ -580,12 +579,12 @@ export default function BasicTabs({
 
   const tabsView = () => {
     return ( <TabList onChange={handleChange} aria-label="lab API tabs example" className='testing-tabList'>
-                {checkPermission('Eminent','Home') && <Tab label="Home" value="home"/>}
-                {checkPermission('Eminent','Allotment') && <Tab label="Allotment" value="allotment"/>}
-                {checkPermission('Eminent','FileStatus') && <Tab label="File Status" value="file_status"/>}
-                {checkPermission('Eminent','MasterOfVacancies') && <Tab label="Master of Vacancies" value="master_of_vacancies"/>}
-                {checkPermission('Eminent','Slotting') && <Tab label="Slotting" value="slotting"/>}
-                {checkPermission('Eminent','GomManagement') && <AntTab label="GoM MANAGEMENT" value="gom_management"/>}
+                {checkPermission('Home','View') && <Tab label="Home" value="home"/>}
+                {checkPermission('Allotment','View') && <Tab label="Allotment" value="allotment"/>}
+                {checkPermission('FileStatus','View') && <Tab label="File Status" value="file_status"/>}
+                {checkPermission('MasterOfVacancies','View') && <Tab label="Master of Vacancies" value="master_of_vacancies"/>}
+                {checkPermission('Slotting','View') && <Tab label="Slotting" value="slotting"/>}
+                {checkPermission('GomManagement','View') && <AntTab label="GoM MANAGEMENT" value="gom_management"/>}
           </TabList>)
   }
 
@@ -599,34 +598,39 @@ export default function BasicTabs({
             {tabsView()}
             {buttonContent}
           </Box>
-          <TabPanel value="home">
+          {checkPermission('Home','View') && <TabPanel value="home">
             <HomeTable
-              filterString={filterString}
-              tabId={value}
-              clearFilter={clearFilter}
+                filterString={filterString}
+                tabId={value}
+                clearFilter={clearFilter}
             />
-          </TabPanel>
+          </TabPanel>}
 
-          <TabPanel value="allotment">
-            <Allotment filterString={filterString} tabId={value} />
-          </TabPanel>
-          <TabPanel value="file_status">
-            <FileStatus filterString={filterString} tabId={value} openFilter={openFilter} />
-          </TabPanel>
-          <TabPanel value="master_of_vacancies">
-            <MasterVacancies filterString={filterString} tabId={value} />
-          </TabPanel>
-          <TabPanel value="slotting">
-            <SlottingTabPage filterString={filterString} tabId={value} />
-          </TabPanel>
-          <TabPanel value="gom_management">
+          {checkPermission('Allotment','View') && <TabPanel value="allotment">
+            <Allotment filterString={filterString} tabId={value}/>
+          </TabPanel>}
+
+          {checkPermission('FileStatus','View') && <TabPanel value="file_status">
+            <FileStatus filterString={filterString} tabId={value} openFilter={openFilter}/>
+          </TabPanel>}
+
+          {checkPermission('MasterOfVacancies','View') && <TabPanel value="master_of_vacancies">
+            <MasterVacancies filterString={filterString} tabId={value}/>
+          </TabPanel>}
+
+          {checkPermission('Slotting','View') && <TabPanel value="slotting">
+            <SlottingTabPage filterString={filterString} tabId={value}/>
+          </TabPanel>}
+
+          {checkPermission('GomManagement','View') && <TabPanel value="gom_management">
             <GomPage
-              filterString={filterString}
-              clearFilter={clearFilter}
-              tabId={value}
-              callFetchFunction={callFetchFunction}
+                filterString={filterString}
+                clearFilter={clearFilter}
+                tabId={value}
+                callFetchFunction={callFetchFunction}
             />
-          </TabPanel>
+          </TabPanel>}
+
         </TabContext>
 
       <Modal
